09e062e419952cf1d6520d1492c5364a51fef02a
Ryo <ryo.sblz415@gmail.com>
2023-12-04 23:09:06 +0900

    1204

--- co_cor.R
+++ co_cor.R
@@ -1,6 +1,7 @@
 cwd <- getwd()
 setwd(paste(cwd, "/input", sep = ""))
 library(tuneR)
+library(ggplot2)
 
 win_ln <- 2 / 1000 * 48000 ## sample
 
@@ -26,8 +27,21 @@ for (fname in fnames) {
   }
   t <- seq(0, ln / fs, length = ln / win_ln)
   setwd(paste(cwd, "out", sep = "/"))
-  png(sprintf("%s.png", substring(fname, 1, (nchar(fname) - 4))), width = 1080, height = 1080)
-  par(mgp = c(2.3, 0.7, 0))
-  plot(t, cor_dat * (-1), xlim = c(0, 1), type = "p", ylab = "IAC", xlab = "time(sec)", cex.lab = 1.7, cex.axis = 1.5, ylim = c(-1, 1)) # ,main = sprintf("%s", fname))
-  dev.off()
+  p <- ggplot() +
+    geom_point(aes(x = t, y = cor_dat), size = 1.5) +
+    labs(x = "Time (sec)", y = "IAC") +
+    xlim(0, 1) +
+    theme_bw() +
+    theme(
+      axis.title = element_text(size = 24), # 軸のタイトルのサイズ
+      axis.text = element_text(size = 21) # 軸のテキストのサイズ
+    )
+
+  ggsave(sprintf("%s.png", substring(fname, 1, (nchar(fname) - 4))), plot = p, width = 10, height = 10, dpi = 300)
+
+  # png(sprintf("%s.png", substring(fname, 1, (nchar(fname) - 4))), width = 1080, height = 1080)
+  # par(mgp = c(2.3, 0.7, 0))
+  # plot(t, cor_dat * (-1), xlim = c(0, 1), type = "p", ylab = "IAC", xlab = "time(sec)", cex.lab = 1.7, cex.axis = 1.5, ylim = c(-1, 1)) # ,main = sprintf("%s", fname))
+  # dev.off()
 }
+setwd(cwd)
